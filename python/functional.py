fc96a9b01ad09d067bb7d963a046496d5ee8cf76
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-05-23 21:37:21 +0530

    reduce and map done

--- python/functional.py
+++ python/functional.py
@@ -1,7 +1,17 @@
 # lmbda function
-fun = lambda x: x*x
-
-
-
+def fun(x): return x*x
 print(fun(5))
 
+print("---------------------")
+
+# 2nd argument can be a collection (ds) and runs the first functiom in each item of that ds
+length = map(len, ['vishal', 'honey', 'gaurav', 'subham'])
+print(list(length))
+
+print('----------------------')
+
+# reduce function
+from functools import reduce
+
+total = reduce(lambda a, x: x + a, [0, 1, 2, 3, 4, 5]);
+print(total)
