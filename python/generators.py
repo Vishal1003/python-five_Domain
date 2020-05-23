e7ad725012b11bb7a1cc4f725b49e2d61bffad43
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-05-24 00:09:50 +0530

    generators

--- python/generators.py
+++ python/generators.py
@@ -14,5 +14,28 @@ def my_gen():
 
 func = my_gen()
 print(next(func))
-
 print(next(func))
+
+
+print("----------------------")
+
+# reversing the string
+def rev_str(s):
+    length = len(s)
+    for i in range(length -1, -1, -1):
+        yield s[i]
+
+
+for char in rev_str("VISHAL"):
+    print(char)
+
+
+print("----------------------")
+
+# generating expressions
+my_li = [1, 3, 6, 10]
+
+ans = (x**2 for x in my_li) 
+print(next(ans))
+print(next(ans))
+print(next(ans))
