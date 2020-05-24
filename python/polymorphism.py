55911d662db379b952a17551bc3fb5861eacd75f
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-05-25 00:29:54 +0530

    polymorphism

---
+++ python/polymorphism.py
@@ -0,0 +1,33 @@
+# multiple forms
+
+class Duck :
+    def quack(self):
+        print("Duck's voice")
+
+    def feathers(self):
+        print("It has white feathers")
+
+
+class Person:
+    def quack(self):
+        print("The peron can imitate duck")
+    
+    def feathers(self):
+        print("Person picks the feather")
+    
+    def name(self):
+        print("Mark Twain")
+    
+
+# polymorphism
+def in_forest(obj):
+    obj.quack()
+    obj.feathers()
+
+donald = Duck()
+vishal = Person()
+
+print(in_forest(donald))
+print("-----------------------------")
+print(in_forest(vishal))
+
