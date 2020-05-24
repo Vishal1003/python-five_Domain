d79095bb725bd66c6c5700d3a755093277cf13f5
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-05-25 00:06:09 +0530

    monkey Patching

--- python/inheritance.py
+++ python/inheritance.py
@@ -19,3 +19,30 @@ class Square(Rectangle):
 
 s = Square(5)
 print('The are of the square is {} and perimeter is {}'.format(s.area(), s.perimeter()))
+
+
+print("-----------------------------")
+
+# Monkey Patching
+# Adding some properties in the class without altering the original class
+
+class A:
+    def __init__(self, num):
+        self.num = num
+    
+    def add(self, num2):
+        return num2 + self.num
+
+def get_num(self):
+    return self.num
+
+# MP
+A.get_num = get_num
+
+obj = A(42)
+print(obj.get_num())
+
+obj2 = A(22)
+print(obj2.get_num())
+
+
