0352d71e2d50ee822c6e7f8a1a8792b65e9e9445
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-05-26 00:20:58 +0530

    web scrapping

---
+++ web-scraping/ws_basics.py
@@ -0,0 +1,15 @@
+# Web scraping is an automated, programmatic process through which data can be constantly 'scrapped' off webpages.
+
+"""
+To extract data using web scraping with python
+
+STEPS:
+
+1. Find the url that you want to scrape.
+2. Inspecting the page (useful data)
+3. Find the data you want to extract
+4. Write a code.
+5. Run the code and extract the data.
+6. Store the data in the required format 
+
+"""
