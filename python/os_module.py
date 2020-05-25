c619cf426a74bdeaf5edd3a743ba7676175c5e3b
Vishal Jha <44357051+Vishal1003@users.noreply.github.com>
2020-05-25 23:45:58 +0530

    heapq

---
+++ python/os_module.py
@@ -0,0 +1,12 @@
+# We can make directories or remove one using this
+import os 
+
+# => making directory
+# os.mkdir("mydir subdir1")
+
+# => get current working directory
+# print(os.getcwd())
+
+# => remove a directory
+# os.rmdir("mydir subdir1")
+
